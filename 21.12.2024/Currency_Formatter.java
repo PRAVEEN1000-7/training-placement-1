bb03601a228ab7f7f537d927a426b34b9b175637
PRAVEEN S <151344150+PRAVEEN1000-7@users.noreply.github.com>
2024-12-21 18:39:26 +0530

    Create Currency_Formatter.java

---
+++ 21.12.2024/Currency_Formatter.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.text.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+        NumberFormat usFormat = NumberFormat.getCurrencyInstance(Locale.US);
+        NumberFormat indiaFormat = NumberFormat.getCurrencyInstance(new Locale("en", "IN"));
+        NumberFormat chinaFormat = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        NumberFormat franceFormat = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        System.out.println("US: " + usFormat.format(payment));
+        System.out.println("India: " + indiaFormat.format(payment));
+        System.out.println("China: " + chinaFormat.format(payment));
+        System.out.println("France: " + franceFormat.format(payment));
+    }
+}
