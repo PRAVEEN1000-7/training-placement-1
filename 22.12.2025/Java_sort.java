d66d9b1230e1b0a523415e157832b6e0ed555e62
PRAVEEN S <151344150+PRAVEEN1000-7@users.noreply.github.com>
2025-12-29 15:25:33 +0530

    Add student sorting functionality by CGPA and name
    Implement a sorting algorithm for students based on CGPA, name, and ID.

---
+++ 22.12.2025/Java_sort.java
@@ -0,0 +1,47 @@
+""" Q : Sort a list of students by CGPA (descending), then by first name (alphabetical order), and finally by ID (ascending)."""
+
+import java.io.*;
+import java.util.*;
+
+class Student {
+    int id;
+    String name;
+    double cgpa;
+
+    Student(int id, String name, double cgpa) {
+        this.id = id;
+        this.name = name;
+        this.cgpa = cgpa;
+    }
+}
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        List<Student> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int id = sc.nextInt();
+            String name = sc.next();
+            double cgpa = sc.nextDouble();
+            list.add(new Student(id, name, cgpa));
+        }
+
+        Collections.sort(list, (a, b) -> {
+            if (Double.compare(b.cgpa, a.cgpa) != 0)
+                return Double.compare(b.cgpa, a.cgpa);
+            if (!a.name.equals(b.name))
+                return a.name.compareTo(b.name);
+            return a.id - b.id;
+        });
+
+        for (Student s : list) {
+            System.out.println(s.name);
+        }
+
+        sc.close();
+    }
+}
